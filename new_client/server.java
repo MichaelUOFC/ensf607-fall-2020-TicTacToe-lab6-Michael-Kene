ccc374bbdadacc41820b600c8fd08cc2485bb404
MichaelUOFC <oluwasegun.adelure@ucalgary.ca>
2020-11-10 00:09:36 -0700

    new client server added

--- new_client/server.java
+++ new_client/server.java
@@ -6,12 +6,12 @@ package new_client;
  * AUthors: Kenechukwu Nwabueze & Michael Adelure
  * 
  */
-import java.io.*;
-import java.net.*;
-import java.util.*;
-import java.awt.*;
-import javax.swing.*;
-import java.io.InputStream.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 public class server
 {
 	private Socket aSocket;
@@ -78,6 +78,13 @@ public class server
 			} // reading from the client
 		}
 	}
+
+	/**
+	 * Server Driver
+	 *
+ 	 * @param args
+	 * @throws IOException
+	 */
 
 	public static void main(String[] args) throws IOException {
 
